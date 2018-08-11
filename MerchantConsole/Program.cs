b24999f313180967fc69478bd7197e16d95f76aa
Unknown <vorwald2006@yahoo.com>
2018-08-11 16:01:55 -0500

    refactored the newing up of Shop and MenuHandler in Program.cs

--- MerchantConsole/Program.cs
+++ MerchantConsole/Program.cs
@@ -8,24 +8,25 @@ namespace MerchantConsole
         private static void Main()
         {
             var player = new Player {Gold = 15000};
+            var shop = new Shop();
+            var menuHandler = new MenuHandler();
 
-            VisitMerchant(player);
+            VisitMerchant(player, shop, menuHandler);
         }
 
-        private static void VisitMerchant(Player player)
+        private static void VisitMerchant(Player player, Shop shop, MenuHandler menuHandler)
         {
-            var shop = new Shop();
-            var menu = new MenuHandler();
+            //Initializing to "Buy" so that menu does not exit right away
             var merchantAction = MerchantAction.Buy;
 
             while (merchantAction != MerchantAction.Exit)
             {
-                merchantAction = menu.GetMerchantAction(player.Gold);
+                merchantAction = menuHandler.GetMerchantAction(player.Gold);
 
                 switch (merchantAction)
                 {
                     case MerchantAction.Buy:
-                        PerformBuyingOption(shop, player);
+                        PerformBuyingOption(shop, player, menuHandler);
                         break;
                     case MerchantAction.Sell:
                         if (!player.Inventory.Any())
@@ -33,22 +34,23 @@ namespace MerchantConsole
                             Console.WriteLine("I am sorry, but it looks like you have nothing to sell.");
                             continue;
                         }
-                        PerformSellback(player);
+                        PerformSellback(player, menuHandler);
                         break;
                     case MerchantAction.Exit:
-                        Console.WriteLine("Thank you for your business! Let's shop again sometime! Press any key to exit.");
-                        Console.ReadKey();
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
             }
+            
+            Console.WriteLine("Thank you for your business! Let's shop again sometime! Press any key to exit.");
+            Console.ReadKey();
         }
 
-        private static void PerformBuyingOption(Shop shop, Player player)
+        private static void PerformBuyingOption(Shop shop, Player player, MenuHandler menuHandler)
         {
-            var menuHandler = new MenuHandler();
-            var buyingResult = menuHandler.GetItemToBuy(shop.GetAllItems());
+            var shopItems = shop.GetAllItems();
+            var buyingResult = menuHandler.GetItemToBuy(shopItems);
             
             if (buyingResult.IsExitingMenu)
                 return;
@@ -64,9 +66,8 @@ namespace MerchantConsole
             Console.WriteLine($"Congratulations! You have bought a brand new {buyingResult.ChosenItem.Name}!");
         }
 
-        private static void PerformSellback(Player player)
+        private static void PerformSellback(Player player, MenuHandler menuHandler)
         {
-            var menuHandler = new MenuHandler();
             var sellBackResult = menuHandler.GetItemToSell(player.Inventory);
 
             if (sellBackResult.IsExitingMenu)
